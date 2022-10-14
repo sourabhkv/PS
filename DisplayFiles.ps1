12caf2e75f465646bccfc892b4f135b43659653a
Sourabh kumar verma <55890376+sourabhkv@users.noreply.github.com>
2022-10-14 20:17:24 +0530

    Update DisplayFiles.ps1

--- DisplayFiles.ps1
+++ DisplayFiles.ps1
@@ -1,4 +1,4 @@
-<#Display files from a directory with specific extension#>
+Write-Host "Display files from a directory with specific extension"
 $loc = Read-Host -Prompt 'Input your location'
 $ext = Read-Host -Prompt 'Input file extension (eg: .pdf)'
 Get-ChildItem -Path $loc | Where {($_.Length -gt '10000')} | Sort {$_.Length} | Where {$_.Extension -like $ext}
